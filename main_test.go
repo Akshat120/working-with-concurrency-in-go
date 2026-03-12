962f815ebdc2bd6cf835b76b8029f1468f5d36bb
Akshat Dhiman <akshat.dhiman15@gmail.com>
2026-03-12 21:24:21 +0530

    Attempt#2

--- main_test.go
+++ main_test.go
@@ -8,33 +8,6 @@ import (
 	"testing"
 )
 
-func Test_PrintMessage(t *testing.T) {
-	stdOut := os.Stdout
-
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	var wg sync.WaitGroup
-
-	msg = "Hello, world!"
-	testMessage := "test-message"
-
-	wg.Add(1)
-	go updateMessage(testMessage, &wg)
-	wg.Wait()
-	printMessage()
-
-	_ = w.Close()
-
-	bytes, _ := io.ReadAll(r)
-	result := string(bytes)
-	os.Stdout = stdOut
-
-	if !strings.Contains(result, testMessage) {
-		t.Errorf("Expected to match test-message, but failed")
-	}
-}
-
 func Test_UpdateMessage(t *testing.T) {
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -46,5 +19,52 @@ func Test_UpdateMessage(t *testing.T) {
 
 	if msg != updated {
 		t.Errorf("Expected to match updated, but failed")
+	}
+}
+
+func Test_PrintMessage(t *testing.T) {
+	stdOut := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	msg = "testing"
+	printMessage()
+
+	_ = w.Close()
+
+	bytes, _ := io.ReadAll(r)
+	result := string(bytes)
+	os.Stdout = stdOut
+
+	if !strings.Contains(result, "testing") {
+		t.Errorf("Expected to match test-message, but failed")
+	}
+}
+
+func Test_main(t *testing.T) {
+	stdOut := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	_ = w.Close()
+
+	bytes, _ := io.ReadAll(r)
+	result := string(bytes)
+	os.Stdout = stdOut
+
+	if !strings.Contains(result, "Hello, universe!") {
+		t.Errorf("Expected to contain \"Hello, universe!\", but failed")
+	}
+
+	if !strings.Contains(result, "Hello, cosmos!") {
+		t.Errorf("Expected to contain \"Hello, cosmos!\", but failed")
+	}
+
+	if !strings.Contains(result, "Hello, world!") {
+		t.Errorf("Expected to contain \"Hello, world!\", but failed")
 	}
 }
